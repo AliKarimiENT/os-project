489d2e81082ad50323dd45192bcf8c5a679b0db8
mehrn79 <mehran.advand500@gmail.com>
2022-01-02 15:15:40 +0330

    ali_2 commit

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ public class Main {
         processes[0] = new Process(1,10,0,0);
         processes[1] = new Process(2,5,0,0);
         processes[2] = new Process(3,8,0,0);
-        FIFO fifo_alg = new FIFO(processes);
+        FIFO fifo_alg = new FIFO(processes) ;
     }
 }
